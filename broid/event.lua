8eea569ae19fc2498f580ad304a259daa3bdb522
miagobble <miagobble@duck.com>
2026-04-09 11:44:01 -0400

    Adjusted symbol names

--- broid/event.lua
+++ broid/event.lua
@@ -3,13 +3,13 @@ local event = {}
 -- Imports
 local symbol = require("broid.modules.symbol")
 
-event.mouseButton1Down = symbol.new("MouseButton1Down")
-event.mouseButton1Up = symbol.new("MouseButton1Up")
-event.mouseButton2Down = symbol.new("MouseButton2Down")
-event.mouseButton2Up = symbol.new("MouseButton2Up")
-event.mouseButton3Down = symbol.new("MouseButton3Down")
-event.mouseButton3Up = symbol.new("MouseButton3Up")
-event.mouseEnter = symbol.new("MouseEnter")
-event.mouseLeave = symbol.new("MouseLeave")
+event.mouseButton1Down = symbol.new("mouseButton1Down")
+event.mouseButton1Up = symbol.new("mouseButton1Up")
+event.mouseButton2Down = symbol.new("mouseButton2Down")
+event.mouseButton2Up = symbol.new("mouseButton2Up")
+event.mouseButton3Down = symbol.new("mouseButton3Down")
+event.mouseButton3Up = symbol.new("mouseButton3Up")
+event.mouseEnter = symbol.new("mouseEnter")
+event.mouseLeave = symbol.new("mouseLeave")
 
 return event
